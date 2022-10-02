9621991513a3d1a439aadf8d118f6cb67324ba81
Alex Curtiss <apccurtiss@gmail.com>
2022-10-02 14:44:42 -0600

    Renamed 'final.csci3403.com' to 'midterm.csci3403.com' because I totally remember which exam this is

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 # These are the steps taken to initialize the OpenVPN volume. It is not necessary to re-run them.
-docker compose run --rm vpn ovpn_genconfig -u udp://final.csci3403.com
+docker compose run --rm vpn ovpn_genconfig -u udp://midterm.csci3403.com
 docker compose run --rm vpn ovpn_initpki
 
 docker compose run --rm vpn easyrsa build-client-full admin nopass
