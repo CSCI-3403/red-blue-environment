6d2545036bde7effc70cef550000e46fc5905078
Alex Curtiss <apccurtiss@gmail.com>
2022-10-08 12:29:09 -0600

    Got the database working and cleaned up volumes

---
+++ volumes/database/create_flag.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+   CREATE USER dev WITH PASSWORD '{flag1-internal-use-only}';
+   CREATE USER alice WITH PASSWORD 'alice';
+
+   CREATE TABLE customers (
+      name TEXT
+   );
+
+   CREATE TABLE creditcards (
+      customer TEXT,
+      cardno TEXT
+   );
+
+   CREATE TABLE orders (
+      name TEXT,
+      product TEXT
+   );
+
+   INSERT INTO customers VALUES ('eve');
+   INSERT INTO creditcards VALUES ('eve', '{flag5-test}');
+   INSERT INTO orders VALUES ('eve', 'tiny-hat-1');
+
+   GRANT CONNECT ON DATABASE sales TO dev, alice;
+   GRANT SELECT ON customers,creditcards,orders TO dev, alice;
+EOSQL
