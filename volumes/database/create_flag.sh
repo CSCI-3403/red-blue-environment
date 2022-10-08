5bf8c2f0a3cece516ac0abc3f391b01b31bfcab5
Alex Curtiss <apccurtiss@gmail.com>
2022-10-08 13:09:40 -0600

    Updated database users.

--- volumes/database/create_flag.sh
+++ volumes/database/create_flag.sh
@@ -3,7 +3,9 @@ set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
    CREATE USER dev WITH PASSWORD '{flag1-internal-use-only}';
-   CREATE USER alice WITH PASSWORD 'alice';
+   CREATE USER alice WITH PASSWORD '{flag3-Sw0rdf1sh}';
+   CREATE USER bob WITH PASSWORD '{flag2-L33T}';
+   CREATE USER carol WITH PASSWORD 'letmein';
 
    CREATE TABLE customers (
       name TEXT
