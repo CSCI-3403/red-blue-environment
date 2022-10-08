0182f4b0b74d5266cbed0d03bf41d9fd384a079c
Alex Curtiss <apccurtiss@gmail.com>
2022-10-08 12:38:19 -0600

    Added script to generate flag #4

--- docker/webserver/bin/add_users.sh
+++ docker/webserver/bin/add_users.sh
@@ -11,4 +11,9 @@ for creds in "${split[@]}"; do
     useradd -mk /etc/skel/ "$user"
     usermod --shell "/bin/bash" "$user"
     echo "$user:$pass" | chpasswd
+
+    echo "Inserting private key flag"
+    mkdir "/home/$user/.ssh"
+    ssh-keygen -b 2048 -t rsa -f "/home/$user/.ssh/id_rsa" -q -N ""
+    echo "{flag4-pr1v4tek3y}" > "/home/$user/.ssh/id_rsa"
 done
