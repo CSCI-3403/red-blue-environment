e29370014f1182e6714c82baf9200f4453e7587c
Alex Curtiss <apccurtiss@gmail.com>
2022-10-09 14:23:22 -0600

    Made the VPN split tunnel

--- generate_cert.sh
+++ generate_cert.sh
@@ -1,4 +1,20 @@
 name=$1
 
 docker compose run --rm vpn easyrsa build-client-full "$name" nopass
-docker compose run --rm vpn ovpn_getclient "$name" > "$name.ovpn"
+
+# For some reason, reading pushed routes from the server also prevents the client from accessing
+# sites outside of the VPN. This is probably debuggable, but for time reasons I'm going with the
+# easy solution: Block pulled routes and add our own to every client config file. 
+cat <<EOT >> "$name.ovpn"
+route-nopull
+route 172.16.10.4 255.255.255.255 vpn_gateway
+route 172.16.10.5 255.255.255.255 vpn_gateway
+route 172.16.10.6 255.255.255.255 vpn_gateway
+route 172.16.10.121 255.255.255.255 vpn_gateway
+route 172.16.10.122 255.255.255.255 vpn_gateway
+route 172.16.10.223 255.255.255.255 vpn_gateway
+route 172.16.10.224 255.255.255.255 vpn_gateway
+route 172.16.10.225 255.255.255.255 vpn_gateway
+EOT
+
+docker compose run --rm vpn ovpn_getclient "$name" >> "$name.ovpn"
